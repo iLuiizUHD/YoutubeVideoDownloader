728403f6c5477a3d96f0a34ab1313a595f9aabc1
Luiz A F Gomes <luizgomes749@gmail.com>
2021-03-02 12:32:51 -0300

    Finally Typescript is working, now we can proceed with the development

--- src/app/js/app.ts
+++ src/app/js/app.ts
@@ -1,3 +1,72 @@
-const youtube = "hello";
+import YouTubeDownloadCore from "ytdl-core";
+import fs from "fs-extra";
+import { config } from "dotenv";
+import { google, youtube_v3 } from "googleapis";
+import * as yup from "yup";
 
-export default youtube;
+config();
+
+class YouTubeDownloader {
+  private YouTube: youtube_v3.Youtube;
+  private YouTubeVideoQuery = yup.object().shape({
+    terms: yup.string().trim().required(),
+  });
+  private YouTubeVideoID = yup.object().shape({
+    id: yup
+      .string()
+      .trim()
+      .matches(/([A-Za-z0-9_\-]{11})/g)
+      .required(),
+  });
+
+  constructor() {
+    const API = process.env.API_KEY;
+    this.YouTube = google.youtube({
+      version: "v3",
+      auth: API,
+    });
+  }
+
+  public async Query(query: string): Promise<any> {
+    try {
+      const body = await this.YouTubeVideoQuery.validate({ terms: query });
+
+      const { data } = await this.YouTube.search.list({
+        part: ["id", "snipped"],
+        q: body.terms,
+      });
+
+      return data.items;
+
+      //
+    } catch (error) {
+      // Do nothing
+    }
+  }
+
+  public async Video(video_id: string): Promise<any> {
+    try {
+      const body = await this.YouTubeVideoID.validate({ id: video_id });
+
+      const { data } = await this.YouTube.videos.list({
+        part: ["id", "snipped"],
+        id: [body.id],
+        maxResults: 10,
+      });
+
+      return data.items;
+
+      //
+    } catch (error) {
+      // Do nothing
+    }
+  }
+
+  public Coo(mms: string) {
+    console.log(mms);
+  }
+
+  public async Download(link: string): Promise<any> {}
+}
+
+export default YouTubeDownloader;
--- src/preload.ts
+++
@@ -1,5 +0,0 @@
-window.addEventListener("DOMContentLoaded", () => {
-  Object.assign(document, {
-    $teste: "FUCKE",
-  });
-});
